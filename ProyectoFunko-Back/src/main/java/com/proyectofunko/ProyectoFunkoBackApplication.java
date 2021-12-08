1d7c31f9cf2d2d0a6cbbb0a62de935566a5811fa
Ryoku <leivasebastian98@gmail.com>
2021-12-08 20:20:07 -0300

    Inicio proyecto

---
+++ ProyectoFunko-Back/src/main/java/com/proyectofunko/ProyectoFunkoBackApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofunko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFunkoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFunkoBackApplication.class, args);
+	}
+
+}
---
+++ ProyectoFunko-Back/src/test/java/com/proyectofunko/ProyectoFunkoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofunko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFunkoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
